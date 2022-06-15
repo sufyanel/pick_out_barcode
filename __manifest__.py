add37b4df7681f2df4a2c6903ed938c9d6ce40b5
waseem  abbas <waseem.zafar@axiomworld.net>
2022-06-15 17:40:33 +0500

    [add] pick_out_delivery module

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Pick and Out Barcode",
+
+    'summary': """
+        This module is used to print pick and out barcodes for sale orders.""",
+
+    'description': """
+        This module is used to print pick and out barcodes for sale orders.
+    """,
+
+    'author': "Axiom World",
+    'website': "http://www.axiomworld.net",
+
+    'category': 'Uncategorized',
+    'version': '15.0.1.0.0',
+    'depends': ['stock'],
+    'data': [
+        'report/stock_picking_operation_ext.xml',
+    ],
+}
